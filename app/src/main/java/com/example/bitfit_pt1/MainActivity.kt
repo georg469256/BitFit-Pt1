c2910df151bffec65a129cc994ef3a24d1d329be
georg469256 <michael.george20152@gmail.com>
2022-10-10 16:51:42 -0500

    Added ability to save entries to database and load old entries to database.

--- app/src/main/java/com/example/bitfit_pt1/MainActivity.kt
+++ app/src/main/java/com/example/bitfit_pt1/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import java.time.Instant
@@ -36,6 +39,29 @@ class MainActivity : AppCompatActivity() {
         val diaryTitleEntry: TextView = findViewById(R.id.DiaryTitleEntry)
         val diaryEntry: TextView = findViewById(R.id.DiaryEntry)
         val addButton: Button = findViewById(R.id.addButton)
+        val diaryRV: RecyclerView = findViewById(R.id.DiaryRV)
+        val diaryList: MutableList<DisplayDiary> = listOf<DisplayDiary>().toMutableList()
+        val diaryAdapter = DiaryAdapter(diaryList)
+        diaryRV.adapter = diaryAdapter
+        diaryRV.layoutManager = LinearLayoutManager(this)
+
+        lifecycleScope.launch {
+            (application as ArticleApplication).db.diaryDao().getAllByDateDesc().collect { databaseList ->
+                databaseList.map { entity ->
+                    DisplayDiary(
+                        entity.id,
+                        entity.title,
+                        entity.date,
+                        entity.entry
+                    )
+                }.also { mappedList ->
+                    diaryList.clear()
+                    diaryList.addAll(mappedList)
+                    diaryAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
 
         diaryTitleEntry.addTextChangedListener(object: TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -67,15 +93,19 @@ class MainActivity : AppCompatActivity() {
 
         addButton.setOnClickListener {
             lifecycleScope.launch(IO) {
+                Log.v("database", diaryTitleEntry.text.toString())
+                Log.v("database", diaryEntry.text.toString())
                 (application as ArticleApplication).db.diaryDao().insert(DiaryEntity(
                     diaryTitleEntry.text.toString(),
                     Instant.now(),
                     diaryEntry.text.toString()
                 ))
             }
+            closeKeyboard()
+            ///wait for insert to complete
+            Thread.sleep(500)
             diaryTitleEntry.text = ""
             diaryEntry.text = ""
-            closeKeyboard()
         }
     }
 }
